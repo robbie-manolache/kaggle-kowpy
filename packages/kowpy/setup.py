35b85963607e6c89de99fa48a10dc8edfaab5fb5
robbie-manolache <robert.b.m@outlook.com>
2025-01-20 18:08:50 -0500

    bump kowpy version

--- packages/kowpy/setup.py
+++ packages/kowpy/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="kowpy",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     install_requires=[
-        "tree-sitter",
+        "tree-sitter==0.21.3",
         "pandas>=2.0.0",
     ],
 )
