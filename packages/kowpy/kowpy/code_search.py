2be8b882f5b83955bd2cd62ddc63b6cdbc147cc7
robbie-manolache <robert.b.m@outlook.com>
2025-01-22 21:25:55 -0500

    init code search module with example

---
+++ packages/kowpy/kowpy/code_search.py
@@ -0,0 +1,18 @@
+
+EXAMPLE = """
+some text here
+```json
+{
+    "files": [
+        "a.py",
+        "/Users/jwalls/release/lib/python3.12/site-packages/astroid/nodes/node_classes.py"
+    ],
+    "objects": [
+        "A.name",
+        "_infer_from_values",
+        "_infer"
+    ]
+}
+```
+some more text
+"""
